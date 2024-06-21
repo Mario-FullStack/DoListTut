61e2dc4cc596316c899f6524ec1bb576476663dc
Mario-FullStack <mmadrod@gmail.com>
2024-06-20 19:07:28 -0600

    Commit temporal antes de actualizar Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { ButtonComponent } from './componentes/button/button.component';
+import { TasksComponent } from './componentes/tasks/tasks.component';
+import { TaskItemComponent } from './componentes/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
